d60f93e38ad6de7707cfe7939f4b6ce6eeaf9bf8
Leonardo <a20125980@pucp.pe>
2020-06-15 20:52:50 -0500

    gg la vida =(

--- src/main/java/sw2/lab6/teletok/controller/PostController.java
+++ src/main/java/sw2/lab6/teletok/controller/PostController.java
@@ -1,13 +1,24 @@
 package sw2.lab6.teletok.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import sw2.lab6.teletok.entity.PostComment;
+import sw2.lab6.teletok.entity.User;
+import sw2.lab6.teletok.repository.PostRepository;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class PostController {
 
+    @Autowired
+    PostRepository postRepository;
+
     @GetMapping(value = {"", "/"})
-    public String listPost(){
+    public String listPost(Model model){
+        model.addAttribute("listapost", postRepository.findAll());
         return "post/list";
     }
 
@@ -27,13 +38,17 @@ public class PostController {
     }
 
     @GetMapping("/post/{id}")
-    public String viewPost() {
+    public String viewPost(@RequestParam("id") int id,Model model) {
+        model.addAttribute("post",postRepository.findById(id));
         return "post/view";
     }
-
     @PostMapping("/post/comment")
-    public String postComment() {
-        return "";
+    public String postComment(@RequestParam("comment") String comment, HttpSession session) {
+        PostComment postComment = new PostComment();
+        postComment.setMessage(comment);
+        User user = (User) session.getAttribute("user");
+        postComment.setUser(user);
+        return "redirect:/";
     }
 
     @PostMapping("/post/like")
